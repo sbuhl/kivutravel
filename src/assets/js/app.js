a705e8e0bd05d7365ec6a2e5b3e77eb9b7d66090
Cerrer <s.buhl@hotmail.com>
2018-09-21 07:38:11 +0200

    clean testimonies

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,6 +8,20 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
+$(document).ready(function(){
+    $.getJSON('https://codepen.io/SebastienCerrer/pen/YOBREQ.js')
+    .done( function(response) {
+        var testyList = '';
+        $.each(response, function(index, testy ) {
+        testyList += "<div class='card-user-profile'><div class='card-section'><p class='card-user-profile-status'>";
+        testyList += testy.comment + "</p><p class='card-user-profile-name'>";
+        testyList += testy.author + "</p><p class='card-user-profile-info'>";
+        testyList += testy.origin + "</p></div></div>";
+        });
+        $('#testy').html(testyList);
+    })
+});
+
 
 $(document).foundation();
 
